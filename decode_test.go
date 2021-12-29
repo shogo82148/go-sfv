c1795df490d9686ed42a43acf273276e0f746a45
Ichinose Shogo <shogo82148@gmail.com>
2021-12-29 20:38:08 +0900

    add generated tests

--- decode_test.go
+++ decode_test.go
@@ -6,17 +6,26 @@ func TestDecodeNumber(t *testing.T) {
 	runTestCases(t, "./testdata/number.json")
 }
 
+func TestDecodeNumberGenerated(t *testing.T) {
+	runTestCases(t, "./testdata/number-generated.json")
+}
+
 func TestDecodeString(t *testing.T) {
 	runTestCases(t, "./testdata/string.json")
 }
 
-// func TestDecodeStringGenerated(t *testing.T) {
-// 	runTestCases(t, "./testdata/string-generated.json")
-// }
+func TestDecodeStringGenerated(t *testing.T) {
+	runTestCases(t, "./testdata/string-generated.json")
+}
 
 func TestDecodeToken(t *testing.T) {
 	runTestCases(t, "./testdata/token.json")
 }
+
+// TODO: fix me
+// func TestDecodeTokenGenerated(t *testing.T) {
+// 	runTestCases(t, "./testdata/token-generated.json")
+// }
 
 func TestDecodeBinary(t *testing.T) {
 	runTestCases(t, "./testdata/binary.json")
